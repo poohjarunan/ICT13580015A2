95983a22583fb5b5a5322cbd271067f361e8ee00
Attachai <sawangwong_a@silpakorn.edu>
2017-12-15 20:38:31 +0700

    init

---
+++ ICT13580015A2/Helpers/DbHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ICT13580015A2.Models;
+using SQLite;
+namespace ICT13580015A2.Helpers
+{
+	public class DbHelper
+	{
+		SQLiteConnection db;
+		public DbHelper(String dbPath)
+
+		{
+			db = new SQLiteConnection(dbPath);
+			db.CreateTable<Product>();
+		}
+
+		public int AddProduct(Product product)
+		{
+			db.Insert(product);
+			return product.id;
+
+		}
+
+        public List<Product> GetProducts()
+        {
+            return db.Table<Product>().ToList();
+
+        }
+
+        public int UpdateProduct(Product product)
+        {
+            return db.Update(product);
+
+        }
+
+        public int DeleteProduct(Product product)
+        {
+            return db.Delete(product);
+
+        }
+	}
+}
---
+++ ICT13580015A2/ProductNewPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using ICT13580015A2.Models;
+using Xamarin.Forms;
+namespace ICT13580015A2
+{
+	public partial class ProductNewPage : ContentPage
+	{
+        Product product;
+
+        public ProductNewPage(Product product = null)
+		{
+			InitializeComponent();
+
+            this.product = product;
+
+            titleLabel.Text = product == null ? "เพิ่มสินค้าใหม่" : "แก้ไขข้อมูลสินค้า";
+
+
+			okButton.Clicked += OkButton_Clicked;
+			cancelButton.Clicked += CancelButton_Clicked;
+
+			categoryPicker.Items.Add("เสื้อ");
+			categoryPicker.Items.Add("กางเกง");
+			categoryPicker.Items.Add("รองเท้า");
+            categoryPicker.Items.Add("ถุงเท้า");
+
+            if (product != null)
+            {
+                nameEntry.Text = product.Name;
+                desEntry.Text = product.Descrpition;
+                categoryPicker.SelectedItem = product.Category;
+                productPriceEntry.Text = product.ProductPrice.ToString();
+                sellPriceEntry.Text = product.SellPrice.ToString();
+                stockEntry.Text = product.Stock.ToString();
+            }
+		}
+
+       
+        async void OkButton_Clicked(object sender, EventArgs e)
+		{
+			var isOk = await DisplayAlert("ยืนยัน", "คุณต้องการบัญทึกใช่หรือไม่", "ใช่", "ไม่ใช่");
+			if (isOk)
+			{
+                if (product == null)
+                {
+                    product = new Product();
+                    product.Name = nameEntry.Text;
+                    product.Descrpition = desEntry.Text;
+                    product.Category = categoryPicker.SelectedItem.ToString();
+                    product.ProductPrice = decimal.Parse(productPriceEntry.Text);
+                    product.SellPrice = decimal.Parse(sellPriceEntry.Text);
+                    product.Stock = int.Parse(stockEntry.Text);
+                    var id = App.DbHelper.AddProduct(product);
+                    await DisplayAlert("บันทึกสำเร็จ", "รหัสสินค้าของท่านคือ #" + id, "ตกลง");
+                }
+                else
+                {
+					product.Name = nameEntry.Text;
+					product.Descrpition = desEntry.Text;
+					product.Category = categoryPicker.SelectedItem.ToString();
+					product.ProductPrice = decimal.Parse(productPriceEntry.Text);
+					product.SellPrice = decimal.Parse(sellPriceEntry.Text);
+					product.Stock = int.Parse(stockEntry.Text);
+                    var id = App.DbHelper.UpdateProduct(product);
+					await DisplayAlert("บันทึกสำเร็จ", "แก้ไขข้อมูลสินค้าเรียบร้อย" , "ตกลง");
+                }
+                await Navigation.PopModalAsync();
+			}
+		}
+		void CancelButton_Clicked(object sender, EventArgs e)
+		{
+            Navigation.PopModalAsync();
+		}
+	}
+}
